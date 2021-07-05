b983bd7cc13397fdddd4488c1c9891062d6312e1
flumile <millionmiles17@hotmail.com>
2021-07-05 16:29:57 +0700

    almost done

--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
-import allMovies from './books.json'
+import allBooks from './books.json'
 import './main.scss'
+
+console.log(allBooks)
+console.log(allBooks.data.slice(0,5))
+document.addEventListener('DOMContentLoaded',function(){
+    const dataSet=20;
+    const showBooks = document.getElementById('cards')
+    const books = allBooks.data.slice(0,dataSet);
+    
+    books.forEach(num =>{
+        const booksDom= getEachdata(num)
+        showBooks.insertAdjacentHTML('beforeend',booksDom)
+    })
+})
+
+function getEachdata(num) {
+   const booksDom =
+    `<div class="card">
+        <img src="https://images.bwbcovers.com/${num.ImageURL}">
+        <div class="title">${num.Title}</div>
+        <div class="author">${num.Authors[0]}</div>
+        <div class="age">${num.AudienceAge}</div>
+        <div class="price">$${num.ListPrice} <span>usd</span></div>
+    </div>`
+    return booksDom
+}
